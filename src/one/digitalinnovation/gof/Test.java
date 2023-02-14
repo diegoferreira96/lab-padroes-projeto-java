016abcad388c74af2d0e566f8cffbd398cd40031
diegoferreira96 <diegoferreira_devweb@hotmail.com>
2023-02-14 17:11:45 -0300

    entregar

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -51,7 +51,12 @@ public class Test {
 		// Facade
 		
 		Facade facade = new Facade();
-		facade.migrarCliente("Venilton", "14801788");
+		facade.migrarCliente("Venilton","Musculação", "14801788");
+		facade.migrarCliente("Venilton","Musculação", "14801788");
+		facade.migrarCliente("Venilton","Musculação", "14801788");
+		facade.migrarCliente("Venilton","Musculação", "14801788");
+		facade.migrarCliente("Venilton","Musculação", "14801788");
+		facade.migrarCliente("Venilton","Musculação", "14801788");
 	}
 
 }
