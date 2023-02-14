81e39e60ac24b152e063ec6f3e502e4407eb6c15
diegoferreira96 <diegoferreira_devweb@hotmail.com>
2023-02-14 17:07:08 -0300

    entrega

--- src/one/digitalinnovation/gof/facade/Facade.java
+++ src/one/digitalinnovation/gof/facade/Facade.java
@@ -5,10 +5,10 @@ import subsistema2.cep.CepApi;
 
 public class Facade {
 
-	public void migrarCliente(String nome, String cep) {
+	public void migrarCliente(String nome, String treino, String cep) {
 		String cidade = CepApi.getInstancia().recuperarCidade(cep);
 		String estado = CepApi.getInstancia().recuperarEstado(cep);
 		
-		CrmService.gravarCliente(nome, cep, cidade, estado);
+		CrmService.gravarCliente(nome, treino, cep, cidade, estado);
 	}
 }
--- src/subsistema1/crm/CrmService.java
+++ src/subsistema1/crm/CrmService.java
@@ -6,11 +6,13 @@ public class CrmService {
 		super();
 	}
 	
-	public static void gravarCliente(String nome, String cep, String cidade, String estado) {
+	public static void gravarCliente(String nome,String treino, String cep, String cidade, String estado) {
 		System.out.println("Cliente salvo no sistema de CRM:");
-		System.out.println(nome);
-		System.out.println(cep);
-		System.out.println(cidade);
-		System.out.println(estado);
+		System.out.println("Nome: " + nome);
+		System.out.println("Tipo de Treino: " + treino);
+		System.out.println("CEP: " + cep);
+		System.out.println("Cidade: " +cidade);
+		System.out.println("Estado: " + estado);
+		System.out.println("===================");
 	}
 }
